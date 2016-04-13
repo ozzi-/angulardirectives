33c4c38c78ee797a2d67b130cbc073458b154bdc
Oussama Zgheb <oussama.zgheb@compass-security.com>
2016-04-13 15:06:12 +0200

    + added message / alert box

--- directives.js
+++ directives.js
@@ -19,7 +19,26 @@ directivesModule.directive('glyph', function(){
 		scope: {
 			gl: "@gl",
 			cl: "@cl",
-			co: "@co",
+			co: "@co"
 		}
 	}
 });
+
+
+directivesModule.directive('message', function(){
+	return {
+		restrict: 'E',
+		replace: true,
+		scope: {
+			ty: "@ty",
+			msg: "@msg",
+		},
+		link: function(scope, element, attrs){
+			if("closable" in attrs){
+				scope.cl = 'yes';
+			}
+		},
+		templateUrl: 'directives/message.html'
+	}
+});
+
